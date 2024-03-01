68f1350b1c4858a9778ae52f895ae7ea61bc59f6
rithusinshith <rithu.raj@3plearning.com>
2024-03-01 13:07:38 +1100

    updated new tours

--- cypress/e2e/newtours.feature
+++ cypress/e2e/newtours.feature
@@ -1,8 +1,22 @@
 Feature: newtours validation
 
+Background:
+
+@smoke @sanity
 Scenario: Home Page
     Given Open newtours application
-    When provide valid 'selenium' and 'selenium'
+    When I log in as following
+    |userName|password|
+    |selenium|selenium|
+    Then click on submit button
+    And verify the url should contains 'newtours/login_sucess'
+
+
+Scenario: Verifying the Title
+    Given Open newtours application
+    When I log in as following
+    |userName|password|
+    |selenium|selenium|
     Then click on submit button
     And verify the url should contains 'newtours/login_sucess'
    
--- cypress/e2e/newtours/newtours.js
+++ cypress/e2e/newtours/newtours.js
@@ -1,15 +1,37 @@
-import {Given,When,Then, And} from "cypress-cucumber-preprocessor/steps";
+import {Before, After,Given,When,Then, And} from "cypress-cucumber-preprocessor/steps";
+
+
+Before(() =>{
+    cy.reload
+
+})
+
+After (()=>{
+
+    cy.reload()
+
+})
 
 Given("Open newtours application",()=>{
     cy.visit("https://www.demo.guru99.com/test/newtours/")
 
 })
 
-When("provide valid {string} and {string}",(userName,password) =>{
-    cy.get('[name="userName"]').type(userName)
-    cy.get('[name="password"]').type(password)
+// When("provide valid {string} and {string}",(userName,password) =>{
+//     cy.get('[name="userName"]').type(userName)
+//     cy.get('[name="password"]').type(password)
 
+// })
+
+When("I log in as following",(datatable)=>{
+
+    datatable.hashes().forEach(element => {
+        cy.get('[name="userName"]').type(element.userName)
+        cy.get('[name="password"]').type(element.password)
+        
+    });
 })
+
 Then("click on submit button",()=>{
    cy.get('[name="submit"]').click()
 
