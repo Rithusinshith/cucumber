b8da4027f5c7210a2c1ff222dec27f7bd2faec54
rithusinshith <rithu.raj@3plearning.com>
2024-03-06 11:46:27 +1100

    added TC for tariff plan

--- cypress/e2e/telecom/telecom.js
+++ cypress/e2e/telecom/telecom.js
@@ -31,4 +31,52 @@ import {Given,When,Then, And} from "cypress-cucumber-preprocessor/steps";
     cy.findByRole('button', { name: /SUBMIT/i }).click()
 
   })
- 
+  
+
+  And("receive the customer ID",()=>{
+
+   let cellText
+    cy.get('.table-wrapper').within(()=>{
+        cy.get('table>tbody>tr:nth-child(1)>td:nth-child(2) h3').invoke('text').then(text => {
+           cellText = text.trim(); // trim to remove any leading or trailing whitespace
+           
+           console.log(cellText)
+            
+        });    
+        
+    }) 
+
+    cy.get('ul.actions').findByText('Home').click()
+
+  })
+
+  When("click on add Tariff Plan",()=>{
+
+    
+    cy.get('.right').findByRole('link', { name: /Add Tariff Plan/i }).click()
+
+
+  })
+
+  Then("I can fill the  details",()=>{
+
+    cy.findByPlaceholderText('Monthly Rental').type('400')
+    cy.findByPlaceholderText('Free Local Minutes').type('100')
+    cy.findByPlaceholderText('Free International Minutes').type('100')
+    cy.findByPlaceholderText('Free SMS Pack').type('500')
+    cy.findByPlaceholderText('Local Per Minutes Charges').type('1')
+    cy.findByPlaceholderText('Inter. Per Minutes Charges').type('5')
+    cy.findByPlaceholderText('SMS Per Charges').type('1')
+
+
+  })
+
+  And("click the Tariff plans  submit button",()=>{
+
+    cy.findByRole('button', { name: /SUBMIT/i }).click()
+    cy.get('#main').should('contain','Congratulation you add Tariff Plan')
+
+  })
+  
+
+
